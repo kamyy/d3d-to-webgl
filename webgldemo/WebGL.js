64fd9c2579f72beb609384bf5fa8ac88c185cbf9
Kam Y Yip <kyip@ymail.com>
2018-01-23 16:56:14 -0800

    Cannot access local files so use http://localhost/8888 and serve files using python -m SimpleHttpServer 8888.

--- webgldemo/WebGL.js
+++ webgldemo/WebGL.js
@@ -1,7 +1,11 @@
 function main() {
     const gl = document.getElementById('webgl').getContext("webgl");
     if (gl) {
-        const shader = new Shader(gl, 'P3C4.vs.glsl', 'P3C4.fs.glsl');
+        const shader = new Shader(
+            gl, 
+            'http://localhost:8888/P3C4.vs.glsl',
+            'http://localhost:8888/P3C4.fs.glsl'
+        );
         gl.clearColor(0.0, 0.0, 0.0, 1.0);
         gl.clear(gl.COLOR_BUFFER_BIT);
     } else {
