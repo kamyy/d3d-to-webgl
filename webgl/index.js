63541c60f14ba642e5b414b1be2d9559b2988459
Kam Y Yip <kyip@ymail.com>
2018-02-09 15:53:57 -0800

    Remove Texture.js and Material.js. These objects are created from JSON directly. Parse JSON and start setting up the scene.

--- webgl/index.js
+++ webgl/index.js
@@ -1,38 +1,99 @@
-let g_GL = null; // GL context
-
-const g_webGLdemo = new class {
-    constructor() {
-        this.worldSpace = new RefFrame();
-        this.cameras = [];
-        this.activeCamIdx = 0;
-        this.mirrorCamera = null;
-        this.ambientLS = null;
-        this.omniDirLS = null;
-        this.actors = [];
-        this.mirror = null;
-        this.drawWirefrm = false;
-        this.drawNormals = false;
-    }
-
-    get activeCamera() {
-        return this.cameras[this.activeCamIdx]; 
-    }
-
-    init() {
-        const shaderP3C4 = new ShaderP3C4();
-        const shaerrP3N3 = new ShaderP3N3();
-    }
-
-    loadScene() {
-
-    }
-};
+let g_GL = null; // GL rendering context:
+const g_textureMap = new Map();
+const g_materialMap = new Map();
 
 function main() {
     g_GL = document.getElementById('webgl').getContext("webgl");
     if (g_GL) {
+
+        Object.defineProperties(g_GL, { 
+            initScene: {
+                value: function() {
+                    this.worldSpace = new RefFrame();
+                    this.cameras = [];
+                    this.cameraIdx = 0;
+                    this.mirrorCam = null;
+                    this.mirrorObj = null;
+                    this.ambientLS = null;
+                    this.omniDirLS = null;
+                    this.drawWirefrm = false;
+                    this.drawNormals = false;
+                    this.sceneLoaded = false;
+
+                    new ShaderP3C4();
+                    new ShaderP3N3();
+                    new ShaderP3N3T2();
+                }
+            },
+
+            loadScene: {
+                value: function() {
+                    const initTextures = function(json) {
+                        for (t of json.textures) {
+                            g_textureMap.set(t.name, t);
+
+                            t.texture = g_GL.createTexture();
+                            const img = new Image();
+
+                            img.onload = function() {
+                                g_GL.bindTexture(g_GL.TEXTURE_2D, t.texture);
+                                if (t.hasAlpha) {
+                                    g_GL.texImage2D(g_GL.TEXTURE_2D, 0, g_GL.RGBA, g_GL.RGBA, g_GL.UNSIGNED_BYTE, this);
+                                } else {
+                                    g_GL.texImage2D(g_GL.TEXTURE_2D, 0, g_GL.RGB,  g_GL.RGB,  g_GL.UNSIGNED_BYTE, this);
+                                }
+                                g_GL.generateMipmap(g_GL.TEXTURE_2D);
+                            };
+
+                            img.src = 'http://localhost:8888/textures/' + t.name + '.img';
+                        }
+                    };
+
+                    const initMaterials = function(json) {
+                        json.materials.forEach(material => g_materialMap.set(material.name, material));
+                    };
+
+                    const initSceneGraph = function(json) {
+
+                    };
+
+                    const request = new XMLHttpRequest();
+                    request.overrideMimeType("application/json");
+                    request.onreadystatechange = () => {
+                        if (request.readyState === 4 && request.status == 200) {
+                            const json = JSON.parse(request.responseText);
+                            initTextures(json);
+                            initMaterials(json);
+                            initSceneGraph(json);
+                        }
+                    }
+                    request.open('GET', url, true);
+                    request.send();
+                }
+            },
+
+            activeCamera: {
+                get: function() { 
+                    return this.cameras[this.cameraIdx];
+                } 
+            },
+
+            activeCameraIdx: {
+                get: function() { 
+                    return this.cameraIdx;
+                },
+                set: function(idx) { 
+                    this.cameraIdx; = idx;
+                }
+            }
+        });
+
+        g_GL.initScene();
+        g_GL.loadScene('http://localhost:8888/json/goku.json');
+
         g_GL.clearColor(0.0, 0.0, 0.0, 1.0);
         g_GL.clear(g_GL.COLOR_BUFFER_BIT);
+
     } else {
         console.log('Failed to get rendering context for WebGL');
     }
