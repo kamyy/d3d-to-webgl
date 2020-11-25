fe574d46c809798fbd0595feb879e168c9b64004
Kam Y Yip <kam.yin.yip@gmail.com>
2020-11-25 14:16:11 -0800

    Update service worker code.

--- webgl/src/index.js
+++ webgl/src/index.js
@@ -4,16 +4,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 
+import * as serviceWorker from './serviceWorker';
 import ConnectedApp, { reduxStore } from './App.js';
-import registerServiceWorker from './registerServiceWorker.js';
 
 ReactDOM.render(
-    <Provider store={reduxStore}><ConnectedApp/></Provider>, 
+    <React.StrictMode>
+        <Provider store={reduxStore}>
+            <ConnectedApp/>
+        </Provider>
+    </React.StrictMode>, 
     document.getElementById('root')
 );
 
-if (module.hot) {
-    module.hot.accept();
-}
-
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
